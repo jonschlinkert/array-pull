ee2ebad6cbbf388e55f78dbc46d4883d2efbb02e
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-11 20:41:19 -0400

    run update

--- test.js
+++ test.js
@@ -11,18 +11,18 @@
 var should = require('should');
 var pull = require('./');
 
-describe('pull', function () {
-  it('should return unique elements in the given array:', function () {
+describe('pull', function() {
+  it('should return unique elements in the given array:', function() {
     pull(['a', 'a', 'c']).should.eql(['a', 'a', 'c']);
   });
 
-  it('should return only the unique elements from all provided arrays:', function () {
+  it('should return only the unique elements from all provided arrays:', function() {
     pull(['a', 'b', 'c'], 'c').should.eql(['a', 'b']);
     pull(['a', 'b', 'c'], 'b', 'c').should.eql(['a']);
     pull(['a', 'b', 'c'], 'a', 'b', 'c').should.eql([]);
   });
 
-  it('should remove elements from the array in place:', function () {
+  it('should remove elements from the array in place:', function() {
     var arr = ['a', 'b', 'c'];
     pull(arr, 'c').should.eql(['a', 'b']);
     arr.should.eql(['a', 'b']);
